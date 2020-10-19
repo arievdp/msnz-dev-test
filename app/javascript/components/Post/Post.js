f8f979520f0ed7c620a5bb18c66edd2d83f7b6a9
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-10-19 19:40:00 +1300

    Added logic, tested Rails API with axios. React receiving data.

--- app/javascript/components/Post/Post.js
+++ app/javascript/components/Post/Post.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-export default function Post() {
+export default function Post(props) {
+    const [post, setPost] = useState({})
+
+    useEffect(()=> {
+        const slug = props.match.params.slug
+        const url = `/api/v1/posts/${slug}`
+        
+        axios.get(url)
+        .then( resp => console.log(resp))
+    }, [])
+
     return (
         <div>
             I am the posts view
--- app/javascript/components/Posts/Posts.js
+++ app/javascript/components/Posts/Posts.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function Posts() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(()=> {
+        axios.get('/api/v1/posts.json')
+        .then( resp =>  console.log(resp) )
+    }, [posts.length])
+
     return (
         <div>
             I am the posts index
