703dddc9f2390225fa3007d72da820632b2aa6a0
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-10-19 19:33:19 +1300

    Added axios package, added logic to app.js, and test divs to Post.js and Posts.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Post from './Post/Post'
+import Posts from './Posts/Posts'
 
 export default function App() {
     return (
-        <div>
-            Hello World
-        </div>
+        <Switch>
+            <Route exact path="/" component={Posts} />
+            <Route exact path="/posts/:slug" component={Post} />
+        </Switch>
     )
 }
