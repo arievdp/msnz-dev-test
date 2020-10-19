d8d0c3895da349c239f001e5f10325198489a64c
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-10-19 19:55:09 +1300

    Add Material UI AppBar

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Post from './Post/Post'
 import Posts from './Posts/Posts'
+import Link from '@material-ui/core/Link';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    
+  }));
 
 export default function App() {
+    const classes = useStyles();
+
     return (
-        <Switch>
-            <Route exact path="/" component={Posts} />
-            <Route exact path="/posts/:slug" component={Post} />
-        </Switch>
+        <Fragment>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                <Typography variant="h6" color="inherit" noWrap>
+                    Media Suite Blog
+                </Typography>   
+                </Toolbar>
+            </AppBar>
+            <Container>
+            <Switch>
+                <Route exact path="/" component={Posts} />
+                <Route exact path="/posts/:slug" component={Post} />
+            </Switch>
+            </Container>
+        </Fragment>   
     )
 }
